c251bc9067fb3b9497dbfa0c4d67dd1e77d15fc6
Kento Morita <morita@info.mie-u.ac.jp>
2026-06-17 17:08:10 +0900

    Finish??

--- 13_Spring/src/main/java/com/example/demo/HelloController.java
+++ 13_Spring/src/main/java/com/example/demo/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.Hello;
 import com.example.demo.HelloService;
@@ -15,17 +16,22 @@ public class HelloController {
     @GetMapping("/hello0")
     // @ResponseBody    // 文字列を直接表示したい場合に使用する
     public String hello0(Model model) {
-        Hello hello = new Hello("Hello, World!");
+        Hello hello = new Hello("Hello, World! by HelloController.hello0()");
 
         model.addAttribute("hello", hello);
-
         // 使うViewの名前を返す（src/main/resources/templates/hello.html）
         return "hello";
     }
 
+    // @Autowired
+    // private HelloService helloService;
+    private final HelloService helloService;
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
     @GetMapping("/hello1")
     public String hello1(Model model) {
-        Hello hello = HelloService.getHello();
+        Hello hello = helloService.getHello();
         // これ以降はhello0と同じ
         model.addAttribute("hello", hello);
         return "hello";
